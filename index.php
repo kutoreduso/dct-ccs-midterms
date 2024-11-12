7ca6b6c914c0591d70e9b5ba155a38a38c401a3e
kutoreduso <kurtcantiga16@gmail.com>
2024-11-12 22:04:32 +0800

    Login Form

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+
+<form>
+  <div class="p-4">
+  <div class="card col-3 mx-auto">
+  <div class="card-header">
+    Login
+  </div>
+  <div class="card-body">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+  </div>
+</div>
+</form>
