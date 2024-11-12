3837aef1064f95642f41e7a2bb1a4ff029fecdf7
kutoreduso <kurtcantiga16@gmail.com>
2024-11-12 22:32:36 +0800

    add a array for the user

---
+++ function.php
@@ -0,0 +1,13 @@
+<?php 
+
+function getUsers() {
+    return [
+        ["email" => "user1@example.com", "password" => "user1"],
+        ["email" => "user2@example.com", "password" => "user2"],
+        ["email" => "user3@example.com", "password" => "user3"],
+        ["email" => "user4@example.com", "password" => "user4"],
+        ["email" => "user5@example.com", "password" => "user5"]
+    ];
+}
+
+?>
